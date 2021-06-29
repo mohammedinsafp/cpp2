6e9ee04a4eca64f4e9d6774650b787d20b28563d
mohammed insaf p <83545341+mohammedinsafp@users.noreply.github.com>
2021-06-29 14:27:54 +0530

    Create cpp2a.cpp

---
+++ cpp2a.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+       int age;
+       cout<<"Enter Age : ";
+       cin>>age;
+       if(age> 18)
+       cout<<"Eligible for voting";
+	else
+	cout<<"not eligible gor voting";
+return 0;
+}
